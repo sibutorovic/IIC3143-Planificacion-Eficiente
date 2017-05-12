fb34e83db3ccc808fe0041f8e52dd65c36ce74f0
fjvaldes1 <fjvaldes1@uc.cl>
2017-05-12 16:16:56 -0300

    forms plan y unit_plan

--- planificacion_eficiente/db/schema.rb
+++ planificacion_eficiente/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501064932) do
+ActiveRecord::Schema.define(version: 20170501064511) do
 
   create_table "abilities", force: :cascade do |t|
     t.string   "name"
@@ -55,13 +55,6 @@ ActiveRecord::Schema.define(version: 20170501064932) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "learnings", force: :cascade do |t|
-    t.string   "objective"
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "plans", force: :cascade do |t|
     t.string   "grade"
     t.string   "subject"
@@ -69,14 +62,6 @@ ActiveRecord::Schema.define(version: 20170501064932) do
     t.integer  "total_hours"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "published"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "previous_learnings", force: :cascade do |t|
