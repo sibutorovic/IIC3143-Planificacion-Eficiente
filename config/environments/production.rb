157ae91c58f1b0e846a73e0d5518ec2c0358c558
Sebastian Butorovic <sibutorovic@uc.cl>
2017-06-23 06:11:06 -0400

    finally

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.serve_static_assets = true
+  config.cache_classes = true
+  config.serve_static_files = true
+  config.public_file_server.enabled = true
   config.assets.compile = true
-
+  config.assets.digest = true
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -20,14 +22,12 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
