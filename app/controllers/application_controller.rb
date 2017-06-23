1662d3ad644d8de81d8834aed9ba130026ee7b35
Sebastian Butorovic <sibutorovic@uc.cl>
2017-06-23 05:21:50 -0400

    comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,14 +15,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :role
-    #devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      #user_params.permit({ role: ['admin','unit_head','teacher'] },:email, :password, :password_confirmation)
-    #end
-
-    #devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      #user_params.permit({ role: ['admin','unit_head','teacher'] }, :email, :password, :password_confirmation)
-    #end
-
 
   end
 
