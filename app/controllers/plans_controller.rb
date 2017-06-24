4960b3377c52e3793424cf98153f4a2b71747800
Sebastian Butorovic <sibutorovic@uc.cl>
2017-06-24 06:20:05 -0400

    Only show my plans

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -4,7 +4,7 @@ class PlansController < ApplicationController
   # GET /plans
   # GET /plans.json
   def index
-    @plans = Plan.all
+    @plans = Plan.all.where(user_id: current_user.id)
     @shared = SharedPlan.where(user_id: current_user.id)
     @shared_plans = []
     @shared.each do |s| 
